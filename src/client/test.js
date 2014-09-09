e22718c07ccc8f599fc80ad66e853b0068f74926
carsonbarlow <carson@mediaforge.com>
2014-09-08 19:45:05 -0600

    re-coded the battle system to reduce suck.

--- src/client/test.js
+++ src/client/test.js
@@ -1,6 +1,5 @@
 var testing = true;
 
-
 var upgrade = new Upgrade();
 cbt.describe('Upgrade: can_purchase');
 cbt.should('check if you can buy the next tier', true, upgrade.can_purchase(20));
@@ -15,132 +14,229 @@ cbt.should('return the current attack', 20, upgrade.get_attack());
 cbt.describe('Upgrade: purchase');
 cbt.should('return the amount of money it took to make the purchase', 20, upgrade.purchase());
 
+var troopFactory = new TroopFactory();
+cbt.describe('TroopFactory');
+var troop = troopFactory.makeTroop('fire',1);
+cbt.should('make troops with the given kind','fire',troop.kind);
+cbt.should('make muliple troops at a time', 10, troopFactory.makeTroop('water',10).length);
+troopFactory.setAttack('fire',30);
+troop = troopFactory.makeTroop('fire',1);
+cbt.should('make troops with the assigned attack power', 30, troop.attack);
+troopFactory.setAttack('fire',20);
+
 var defenders = new Defenders();
 cbt.describe('Defenders: add_unit');
-defenders.add_unit('fire',10);
-cbt.should('add units of the specified kind',10,defenders.fire);
-cbt.should('not add other units', 0, defenders.wind);
+defenders.add_unit(troopFactory.makeTroop('fire',10));
+cbt.should('add units of the specified kind',10,defenders.fire.length);
+cbt.should('not add other units', 0, defenders.wind.length);
+defenders.add_unit(new Troop('earth', 20));
+cbt.should('add single units too',1,defenders.earth.length);
+defenders.add_unit([new Troop('earth',20), new Troop('water', 20)]);
+cbt.should('add different kinds of troops at the same time', [2,1],[defenders.earth.length,defenders.water.length]);
 cbt.describe('Defenders: clear_units');
 defenders.clear_units();
-cbt.should('remove all units',0, defenders.fire);
+cbt.should('remove all units',0, defenders.fire.length);
+
+cbt.describe('Defenders: remove_unit');
+defenders.add_unit(troopFactory.makeTroop('fire',10));
+troop = defenders.fire[5];
+troop.health = 50;
+var total_health = 0;
+for (var i = 0; i < defenders.fire.length; i++){
+  total_health += defenders.fire[i].health;
+}
+cbt.should('pretest..',950,total_health);
+defenders.remove_unit(troop);
+total_health = 0;
+for (i = 0; i < defenders.fire.length; i++){
+  total_health += defenders.fire[i].health;
+}
+cbt.should('remove the specified unit', 900, total_health);
 
 cbt.describe('Defenders: get_total');
 defenders.clear_units();
-defenders.add_unit('wind', 5);
-defenders.add_unit('water', 3);
-defenders.add_unit('earth', 1);
+defenders.add_unit(troopFactory.makeTroop('wind',5));
+defenders.add_unit(troopFactory.makeTroop('water',3));
+defenders.add_unit(troopFactory.makeTroop('earth',1));
 cbt.should('return the sum of all the units', 9, defenders.get_total());
 
-cbt.describe('Defenders: kill unit');
+cbt.describe('Defenders: damage_kind');
 defenders.clear_units();
-defenders.remove_index = 0;
-defenders.add_unit('wind', 5);
-defenders.add_unit('water', 3);
-defenders.add_unit('earth', 1);
-defenders.kill_unit();
-cbt.should('move index to next unit type', 1, defenders.remove_index);
-cbt.should('kill off one of the next in line', 2, defenders.water);
-defenders.kill_unit();
-cbt.should('skip empty unit types', 3, defenders.remove_index);
-defenders.kill_unit();
-defenders.kill_unit();
-defenders.kill_unit();
-defenders.kill_unit();
-defenders.kill_unit();
-cbt.should('leave the most numerous unit type before total anniallation', 2, defenders.wind);
-cbt.should('not have other kinds of units',0, defenders.water);
-cbt.should('return false if there are any units left', false, defenders.kill_unit());
-cbt.should('return true if all units are eliminated', true, defenders.kill_unit());
+defenders.add_unit(troopFactory.makeTroop('water',3));
+defenders.add_unit(new Troop('fire', 20));
+defenders.damage_kind('water',75);
+var outcome_array = [];
+for (i = 0; i < defenders.water.length; i++){
+  outcome_array.push(defenders.water[i].health);
+}
+outcome_array.push(defenders.fire[0].health);
+cbt.should('evenly distribute damage to all units of that kind',[75,75,75,100],outcome_array);
+defenders.water[1].health = 25;
+defenders.damage_kind('water',150);
+outcome_array = [];
+for (i = 0; i < defenders.water.length; i++){
+  outcome_array.push(defenders.water[i].health);
+}
+outcome_array.push(defenders.fire[0].health);
+cbt.should('kill and remove dead units from the defender\'s object',[25,25,100],outcome_array);
 
-cbt.describe('Defenders: recieve_damage');
+cbt.describe('Defenders: deploy_units');
 defenders.clear_units();
-defenders.remove_index = 0;
-defenders.add_unit('wind', 5);
-defenders.add_unit('water', 3);
-defenders.add_unit('earth', 1);
-defenders.recieve_damage(200);
-cbt.should('remove 2 units if 200 damage was presented', 7, defenders.get_total());
-defenders.recieve_damage(75);
-cbt.should('not kill when non-fatal damage is issued', 7, defenders.get_total());
-defenders.recieve_damage(30);
-cbt.should('kill a unit when it\'s health is low', 6, defenders.get_total());
-cbt.should('roll over damage to the next unit', 95, defenders.defender_health);
-defenders.recieve_damage(10000);
-cbt.should('never kill more units than are defending', 0, defenders.get_total());
+defenders.add_unit(troopFactory.makeTroop('wind',5));
+defenders.add_unit(troopFactory.makeTroop('water',3));
+defenders.add_unit(troopFactory.makeTroop('earth',1));
+defenders.remove_index = 3;
+outcome_array = defenders.deploy_units(6);
+for (i = 0; i < outcome_array.length; i++){
+  outcome_array[i] = outcome_array[i].kind;
+}
+cbt.should('evenly deploy unit types',["wind", "water", "earth", "wind", "water", "wind"],outcome_array);
+cbt.should('remove those units from the defender object', 3, defenders.get_total());
 
-cbt.describe('Defenders: damage_type');
-defenders.clear_units();
-defenders.defender_health = 80;
-defenders.remove_index = 0;
-defenders.add_unit('wind', 5);
-defenders.add_unit('water', 3);
-defenders.add_unit('earth', 1);
-defenders.damage_type('water',200);
-cbt.should('only deal damage to/kill the unit type specified', 1, defenders.water);
-defenders.damage_type('water',85);
-cbt.should('kill off the last of a unit type and roll over the damage', 0, defenders.water);
-cbt.should('..',95,defenders.defender_health);
-defenders.water = 5;
-defenders.damage_type('water', 1000);
-cbt.should('not spill damage onto other units', 5, defenders.wind);
-cbt.should('..', 1, defenders.earth);
-
-cbt.describe('Defenders: remove_unit');
-defenders.earth = 10;
-cbt.should('return the value that was removed', 3, defenders.remove_unit('earth',3));
-cbt.should('leave the remove the units', 7, defenders.earth);
-cbt.should('return only what was removed, not what was asked for', 7, defenders.remove_unit('earth',10));
-
-var player1 = new Player(1);
-var player2 = new Player(2);
+cbt.describe('BattleField: troop_fight');
 var defender1 = new Defenders();
 var defender2 = new Defenders();
-defender1.add_unit('wind',10);
-defender2.add_unit('water',10);
-battleField = new BattleField(player1,player2,defender1,defender2);
+var battleField = new BattleField(defender1,defender2);
+var troop1 = new Troop('fire',20);
+var troop2 = new Troop('water',20);
+
+battleField.troop_fight(troop1,troop2);
+cbt.should('deal damage to one another',[80,80],[troop1.health,troop2.health]);
+troop1.health = 10;
+troop2.health = 100;
+battleField.troop_fight(troop1,troop2);
+cbt.should('never overkill, and mitigate damage accordingly',[0,90],[troop1.health,troop2.health]);
+troop1.health = 10;
+troop2.health = 100;
+troop1.attack = 40;
+battleField.troop_fight(troop1,troop2);
+cbt.should('..',[0,80],[troop1.health,troop2.health]);
+troop1.health = 10;
+troop2.health = 100;
+troop1.attack = 20;
+troop2.attack = 40;
+battleField.troop_fight(troop1,troop2);
+cbt.should('..',[0,95],[troop1.health,troop2.health]);
+
 cbt.describe('BattleField: bane_attack');
+defender1.add_unit(troopFactory.makeTroop('wind',3));
+defender2.add_unit(troopFactory.makeTroop('earth',6));
 battleField.bane_attack();
-cbt.should('damage the units that are weak against the other element',8,defender2.water);
-cbt.should('not damage other units',10,defender1.wind);
+outcome_array = []
+for (i = 0; i < defender1.wind.length; i++){
+  outcome_array.push(defender1.wind[i].health);
+}
+cbt.should('evenly distribute the damage to all opposing kinds',[60,60,60],outcome_array);
+outcome_array = []
+for (i = 0; i < defender2.earth.length; i++){
+  outcome_array.push(defender2.earth[i].health);
+}
+cbt.should('but not kill the kinds that aren\'t weak',[100,100,100,100,100,100],outcome_array);
+defender1.wind[1].health = 10;
+battleField.bane_attack();
+outcome_array = []
+for (i = 0; i < defender1.wind.length; i++){
+  outcome_array.push(defender1.wind[i].health);
+}
+cbt.should('will kill and remove casualties',[20,20],outcome_array);
 
 cbt.describe('BattleField: standard_attack');
 defender1.clear_units();
 defender2.clear_units();
-defender1.add_unit('wind',10);
-defender2.add_unit('water',5);
-defender1.defender_health = 100;
-defender1.defender_health = 100;
-battleField.standard_attack();
-cbt.should('damage both sides indiscriminately',9,defender1.wind);
-cbt.should('..',3,defender2.water);
+defender1.add_unit(troopFactory.makeTroop('fire',5));
+defender2.add_unit(troopFactory.makeTroop('fire',5));
+
+battleField.standard_attack(2);
+outcome_array = []
+for (i = 0; i < defender1.fire.length; i++){
+  outcome_array.push(defender1.fire[i].health);
+}
+cbt.should('deploy the troops and have them attack each other for the specified amount of time',[60,60,60,60,60],outcome_array);
+outcome_array = []
+for (i = 0; i < defender2.fire.length; i++){
+  outcome_array.push(defender2.fire[i].health);
+}
+cbt.should('..',[60,60,60,60,60],outcome_array);
+
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',2));
+defender2.add_unit(troopFactory.makeTroop('fire',2));
+defender1.fire[0].health = 20;
+defender2.fire[0].health = 20;
+battleField.standard_attack(1);
+cbt.should('remove both units that kill each other',[1,1],[defender1.fire.length,defender2.fire.length]);
+
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',3));
+defender2.add_unit(troopFactory.makeTroop('fire',2));
+defender1.fire[2].health = 20;
+battleField.standard_attack(2);
+cbt.should('replace the fallen troops if able',[2,2],[defender1.fire.length,defender2.fire.length]);
+outcome_array = []
+for (i = 0; i < defender1.fire.length; i++){
+  outcome_array.push(defender1.fire[i].health);
+}
+cbt.should('..',[80,60],outcome_array);
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',2));
+defender2.add_unit(troopFactory.makeTroop('fire',3));
+defender1.fire[1].health = 20;
+battleField.standard_attack(2);
+cbt.should('remove non-fighting troops when can\'t replace casualty',[1,3],[defender1.fire.length,defender2.fire.length]);
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',2));
+defender2.add_unit(troopFactory.makeTroop('fire',3));
+defender2.fire[2].health = 20;
+battleField.standard_attack(2);
+cbt.should('replace the fallen troops if able',[2,2],[defender1.fire.length,defender2.fire.length]);
+outcome_array = []
+for (i = 0; i < defender2.fire.length; i++){
+  outcome_array.push(defender2.fire[i].health);
+}
+cbt.should('..',[80,60],outcome_array);
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',3));
+defender2.add_unit(troopFactory.makeTroop('fire',2));
+defender2.fire[1].health = 20;
+battleField.standard_attack(2);
+cbt.should('remove non-fighting troops when can\'t replace casualty',[3,1],[defender1.fire.length,defender2.fire.length]);
 
 cbt.describe('BattleField: resolve_conflict');
 defender1.clear_units();
 defender2.clear_units();
-defender1.add_unit('fire',10);
-defender2.add_unit('water',10);
-defender1.defender_health = 100;
-defender2.defender_health = 100;
+defender1.add_unit(troopFactory.makeTroop('fire',5));
+defender2.add_unit(troopFactory.makeTroop('fire',5));
 battleField.resolve_conflict();
-cbt.should('leave at least one side dead',0,defender1.get_total());
-cbt.should('levy casualties on the other side', 6, defender2.get_total());
+cbt.should('kill both evenly matched forces',[0,0],[defender1.get_total(),defender2.get_total()]);
 defender1.clear_units();
 defender2.clear_units();
-defender1.add_unit('water',10);
-defender2.add_unit('water',10);
-defender1.defender_health = 100;
-defender2.defender_health = 100;
+defender1.add_unit(troopFactory.makeTroop('fire',5));
+defender2.add_unit(troopFactory.makeTroop('fire',5));
+defender1.fire[0].attack = 24;
 battleField.resolve_conflict();
-cbt.should('be able to kill both sides', 0, defender1.get_total());
-cbt.should('..', 0, defender2.get_total());
+cbt.should('leave a forece standing when unevenly matched',[1,0],[defender1.get_total(),defender2.get_total()]);
+cbt.should('..',16,defender1.fire[0].health);
 
-// defender1.clear_units();
-// defender2.clear_units();
-// defender1.defender_health = 100;
-// defender2.defender_health = 100;
-// defender1.add_unit('water',10);
-// defender2.add_unit('water',8);
-// // player1.purchase_upgrade('water');
-// battleField.resolve_conflict();
-// console.log(defender1.get_total() + '.' + defender1.defender_health);
-// console.log(defender2.get_total() + '.' + defender2.defender_health);
+defender1.clear_units();
+defender2.clear_units();
+troopFactory.setAttack('fire',5);
+defender1.add_unit(troopFactory.makeTroop('fire',1));
+defender2.add_unit(troopFactory.makeTroop('fire',5));
+troopFactory.setAttack('fire',20);
+defender1.fire[0].attack = 50;
+battleField.resolve_conflict();
+cbt.should('properly weigh in attack value with health/size of army',[1,0],[defender1.get_total(),defender2.get_total()]);
+cbt.should('..',50,defender1.fire[0].health);
+
+defender1.clear_units();
+defender2.clear_units();
+defender1.add_unit(troopFactory.makeTroop('fire',5));
+defender2.add_unit(troopFactory.makeTroop('water',5));
+battleField.resolve_conflict();
+cbt.should('properly use bane_attack',[0,5],[defender1.get_total(),defender2.get_total()]);
+cbt.should('..',40,defender2.water[0].health);
